7dbc48439aabe2c94d13953c9469f4e72fad1450
Vitor Roque <vitor.roque@netshow.me>
2020-02-07 11:15:19 -0300

    Added tests

--- src/tests/ApplicationTest.php
+++ src/tests/ApplicationTest.php
@@ -1,18 +1,58 @@
 <?php
 namespace Flowee\Tests;
 
+use Exception;
 use Flowee\Core\Application;
 use Flowee\Tests\FloweeTestCase;
+use React\Socket\Connector;
+use React\Socket\ConnectionInterface;
+use React\EventLoop\Factory;
 
 class ApplicationTest extends FloweeTestCase {
 	
 	public function setUp(): void
   {
     // parent set up for our testCase
-		parent::setUp();	
+    parent::setUp();
+  }
+  
+  /** @test **/
+  public function should_init_server() {
+    $this->runServer();
+    $server_log = file_get_contents('src/tests/.output/manager.log');
+    $this->assertNotEmpty($server_log);
+  }
+  /** @test **/
+  public function should_receive_data() {
+    $this->runServer();
+    $data = $this->prepareData('error');
+
+    $socket = stream_socket_client('tcp://127.0.0.1:8000');
+    stream_socket_sendto($socket, json_encode($data));
+
+    sleep(1);
+    $log = file_get_contents('src/tests/.output/manager.log');
+    $this->assertStringContainsString($data->message, $log);
   }
 
-  public function should_receive_error_data() {
-    // make that tests
+  /** @test **/
+  public function should_check_data_correctly(){
+    $this->assertJson(json_encode($this->prepareData('error')));
+  }
+  
+  /**
+   * Preparing a object data
+   * @param String $type
+   * @param String $saving
+   * @return object
+   * **/
+  private function prepareData(string $type, bool $saving = false): object {
+    $arr = (object) [
+      'type' => $type,
+      'save' => $saving,
+      'message' => "default error with type $type ."
+    ];
+
+    return $arr;
   }
 }
--- src/tests/FloweeTestCase.php
+++ src/tests/FloweeTestCase.php
@@ -9,14 +9,17 @@ class FloweeTestCase extends TestCase {
 
 	public function setUp(): void {
 		parent::setUp();
-		$this->runServer();
 	}	
 
 	public function tearDown(): void {
 		$this->stopServer();
 	}
 	
+  protected function shouldSeeManagerOutput($content) {
+    $this->assertContains($content, file_get_contents('src/tests/.output/manager.log'), "Couldn't find '$content' in manager.log");
+  }
 	public function stopServer() {
+    file_put_contents('src/tests/.output/manager.log', '');
 		exec("kill {$this->managerPid} > /dev/null 2>&1");
 	}
 
@@ -24,6 +27,7 @@ class FloweeTestCase extends TestCase {
 		$this->managerPid = exec(
 						'php flowee.php > src/tests/.output/manager.log 2>&1 & echo $!',
 			$output					
-		);
+    );
+    sleep(1);
 	}
 }
