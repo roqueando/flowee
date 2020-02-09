b53871bb6d4539584675096c39c76a67b1521872
Vitor Roque <vitor.roque@netshow.me>
2020-02-08 21:28:51 -0300

    Created log folders

--- src/tests/ApplicationTest.php
+++ src/tests/ApplicationTest.php
@@ -1,12 +1,6 @@
 <?php
 namespace Flowee\Tests;
-
-use Exception;
-use Flowee\Core\Application;
 use Flowee\Tests\FloweeTestCase;
-use React\Socket\Connector;
-use React\Socket\ConnectionInterface;
-use React\EventLoop\Factory;
 
 class ApplicationTest extends FloweeTestCase {
 	
@@ -22,6 +16,7 @@ class ApplicationTest extends FloweeTestCase {
     $server_log = file_get_contents('src/tests/.output/manager.log');
     $this->assertNotEmpty($server_log);
   }
+
   /** @test **/
   public function should_receive_data() {
     $this->runServer();
@@ -33,6 +28,7 @@ class ApplicationTest extends FloweeTestCase {
     sleep(1);
     $log = file_get_contents('src/tests/.output/manager.log');
     $this->assertStringContainsString($data->message, $log);
+    $this->cleanLogs();
   }
 
   /** @test **/
@@ -40,6 +36,54 @@ class ApplicationTest extends FloweeTestCase {
     $this->assertJson(json_encode($this->prepareData('error')));
   }
   
+  /** @test **/
+  public function should_not_save_logs_in_root() {
+    $this->runServer();
+    $data = $this->prepareData('error');
+    $this->createSocketAndSendData($data);
+
+    sleep(1);
+    $files = glob("src/log/*.log");
+    $this->assertEquals(0, count($files));
+  }
+
+  /** @test **/
+  public function should_save_logs_in_respectively_folders() {
+    $this->runServer();
+
+    $data = $this->getAllData();
+    foreach($data as $d => $value) {
+      $this->createSocketAndSendData($value);
+    }
+
+    sleep(1);
+    
+    foreach($data as $log => $content) {
+      $this->assertDirectoryExists("src/log/$log");
+      $files = glob("src/log/$log/*.log");
+      $this->assertGreaterThan(0, $files);
+
+      $filename = $files[0];
+      $this->assertFileExists("$filename");
+      $fileContent = file_get_contents("$filename");
+      $this->assertStringContainsString($content->message, $fileContent);
+      $this->cleanLogs($log);
+    }
+  }
+
+  private function getAllData(): array {
+    return [
+      'errors' => $this->prepareData('error', true),
+      'warnings' => $this->prepareData('warning', true),
+      'successes' => $this->prepareData('success', true),
+      'faileds' => $this->prepareData('fail', true)
+    ];
+  }
+
+  private function createSocketAndSendData(object $data): void {
+    $socket = stream_socket_client('tcp://127.0.0.1:8000');
+    stream_socket_sendto($socket, json_encode($data));
+  }
   /**
    * Preparing a object data
    * @param String $type
